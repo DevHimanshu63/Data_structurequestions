db9216378b846a1eb3b8c94d8093a6a8555e8ffa
DevHimanshu63 <hsc95067@gmail.com>
2022-02-14 15:20:07 +0530

    reverse array

---
+++ array/reverse_Array.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void reverse(int[] a){
+    // write your code here
+    int left= 0;
+    int right= a.length-1;
+    while(left < right){
+        //for swwaping elements
+      int temp=a[left];
+      a[left]=a[right];
+      a[right]=temp;
+
+
+      right--;
+      left++;
+    }
+  
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    reverse(a);
+    display(a);
+ }
+
+}
